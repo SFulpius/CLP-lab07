e87b02b3831f9aab4288b45adbe083a72323ffe3
Elsa Weber <elsa.weber@epfl.ch>
2018-12-30 08:34:41 +0100

    Modify example to better understand the bug

--- examples/PolyType.scala
+++ examples/PolyType.scala
@@ -1,12 +1,17 @@
 object PolyType {
-    abstract class List[A]
-    case class Cons[A](head : A, tail : List[A]) extends List[A]
-    case class Nil[A]() extends List[A]
-    def add[A](elem : A, l : List[A]) : List[A] = {
-        Cons[A](elem, l)
-    }
-val x : List[Int] = Cons[Int]( 2, Nil[Int]());
-x match {
-    case Cons(h,t) => Std.printString(h)
-}
+
+  abstract class List[A]
+
+  case class Cons[B](head: B, tail: List[B]) extends List[B]
+
+  case class Nil[A]() extends List[A]
+
+  def add[C](elem: C, l: List[C]): List[C] = {
+    Cons[C](elem, l)
+  }
+
+  val x: List[Int] = Cons[Int](2, Nil[Int]());
+  x match {
+    case Cons(h, t) => Std.printString(h)
+  }
 }
